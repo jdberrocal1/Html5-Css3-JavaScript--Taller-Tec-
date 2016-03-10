26858bde1bda3046602694a68da5b1053061a7e9
Walter Chavarria <wchavarria-as@avantica.net>
2016-03-10 10:20:53 -0600

    Added Contact Mail Functionality

--- profile/scripts/main.js
+++ profile/scripts/main.js
@@ -1,7 +1,13 @@
-(function(){
 
-	'use strict';
+'use strict';
 
+function sendMail(mailBody) {
+    var link = 'mailto:wchavarria03@gmail.com'
+             + '?cc='+ escape(document.getElementById('contactEmail').value)
+             + '&subject=' + escape('Profile Information Message')
+             + '&body=' + escape( 'User Name:   ' + document.getElementById('contactName').value) + '    \n'
+             + escape( 'User Message:   ' + document.getElementById('contactMessage').value);
 
+    window.location.href = link;
+}
 
-})();
