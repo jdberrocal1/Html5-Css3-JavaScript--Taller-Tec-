d79d91b7305a8ff4e9e6ee448914cf4d345baa91
Walter Chavarria <wchavarria-as@avantica.net>
2016-03-11 15:52:52 -0600

    Add Todos View

--- profile/scripts/main.js
+++ profile/scripts/main.js
@@ -1,6 +1,8 @@
 
 'use strict';
 
+
+/*SendMail*/
 function sendMail(mailBody) {
     var link = 'mailto:wchavarria03@gmail.com'
              + '?cc='+ escape(document.getElementById('contactEmail').value)
@@ -10,4 +12,28 @@ function sendMail(mailBody) {
 
     window.location.href = link;
 }
+
+
+
+/*Technologies*/
+var circles = document.getElementsByClassName('circle');
+
+
+function toggleSkillInfo() {
+	var skillNameEle = this.firstElementChild;
+	var skillPorcentageEle = skillNameEle.nextElementSibling;
+	
+	skillNameEle.className = skillNameEle.className === 'hide' ? '' : 'hide';
+	skillPorcentageEle.className = skillPorcentageEle.className === 'hide' ? '' : 'hide';
+
+	this.className = this.className === 'circle skillActive' ? 'circle': 'circle skillActive';
+
+}
+
+for(var i = circles.length-1; i >= 0; i--) {
+	circles[i].addEventListener('click', toggleSkillInfo, false)
+}
+
+
+
 
