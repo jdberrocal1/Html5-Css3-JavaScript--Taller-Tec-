b3f5247b0f86b8d94f5921f596b25d3e0dd67939
Walter Chavarria <wchavarria03@hotmail.com>
2016-03-15 20:18:13 -0600

    Added project final version under profile folder

--- profile/scripts/main.js
+++ profile/scripts/main.js
@@ -3,12 +3,14 @@
 
 
 /*SendMail*/
+/*Funcion que se llama desde el index, usa el cliente de correo local para enviar un correo*/
 function sendMail(mailBody) {
     var link = 'mailto:wchavarria03@gmail.com'
              + '?cc='+ escape(document.getElementById('contactEmail').value)
              + '&subject=' + escape('Profile Information Message')
              + '&body=' + escape( 'User Name:   ' + document.getElementById('contactName').value) + '    \n'
-             + escape( 'User Message:   ' + document.getElementById('contactMessage').value);
+             + escape( 'User Message:   ' + document.getElementById('contactMessag
+                e').value);
 
     window.location.href = link;
 }
@@ -22,14 +24,15 @@ var circles = document.getElementsByClassName('circle');
 function toggleSkillInfo() {
 	var skillNameEle = this.firstElementChild;
 	var skillPorcentageEle = skillNameEle.nextElementSibling;
-	
+
+    /*Operación ternaria de javascript, es utilizada para hacer toggle de la clase hide*/
 	skillNameEle.className = skillNameEle.className === 'hide' ? '' : 'hide';
 	skillPorcentageEle.className = skillPorcentageEle.className === 'hide' ? '' : 'hide';
 
 	this.className = this.className === 'circle skillActive' ? 'circle': 'circle skillActive';
 
 }
-
+/*Agregamos al evento click la funcion toggleSkill para que cuando se clickee se muestre el porcentaje o se muestre el nombre de la skill*/
 for(var i = circles.length-1; i >= 0; i--) {
 	circles[i].addEventListener('click', toggleSkillInfo, false)
 }
--- profile/scripts/todos.js
+++ profile/scripts/todos.js
@@ -1,10 +1,10 @@
-
 /*ToDo's*/
 
 var myStorage = localStorage;
 var myToDos = [];
 
 function getToDos () {
+	/* Lee el todos del localStorage y lo convierte a formato json */
 	myToDos = JSON.parse(myStorage.getItem('todos')) || [];
 }
 
@@ -12,6 +12,7 @@ function AddToDo() {
 	var todoInputEle = document.getElementById('todoInput');
 	if(todoInputEle.value) {
 		myToDos.push(todoInputEle.value);
+		/*Almacena un item todos en el localStorage, toma el objeto y lo convierte a string*/
 		myStorage.setItem('todos', JSON.stringify(myToDos));
 		todoInputEle.value = '';
 
@@ -20,6 +21,9 @@ function AddToDo() {
 	}
 }
 
+/*
+ *Limpia la lista de todos cuando se agrega uno nuevo para leerlas nuevamente del localStorage
+ */
 function removeOldUI() {
 	var oldTodoList = document.getElementById('todoList');
 	while (oldTodoList.firstChild) {
@@ -30,15 +34,19 @@ function removeOldUI() {
 function checkDone(evt) {
 	var checkbox = evt.target;
 	checkbox.nextElementSibling.className = checkbox.checked===true?'done':'';
-	
+
 }
 
 function refreshToDoUI() {
+	/*
+	* NOTA: Se debe mejorar, haciendo que no sea necesario eliminar los que ya existen.
+	*/
 	removeOldUI();
 
+	/*Fragment de javascript para reducir el repaint en el DOM*/
 	var frag = document.createDocumentFragment();
 	myToDos.forEach(function(item){
-		
+
 		var todoItem = document.createElement('div');
 		todoItem.className = 'todoItem';
 
@@ -58,10 +66,9 @@ function refreshToDoUI() {
 }
 
 
-function completeToDo() {
-
-}
-
+/*Función para agregar dos TODOs en la lista para que la misma no inicie vacia
+NOTA: No es necesario solo es por estetica(No ver la lista vacia)
+*/
 function fillToDos() {
 	if(!myToDos.length) {
 		myToDos.push('Finish the Web Course');
